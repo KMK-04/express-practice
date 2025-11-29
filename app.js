a44cf45710b1279ebef60a4400740778012302e9
김 무겸 <growthmk@ajou.ac.kr>
2025-11-29 16:29:44 +0900

    app.js create

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express')
+const logger = require('morgan')
+const app = express()
+const port = 3000
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(logger('dev'))
+
+app.post('/user', (req, res) => {
+    console.log(req.body.name)
+    res.send(req.body)
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/user/:id', (req, res) => {
+    res.send(`User id is ${req.params.id}`)
+})
+
+app.get('/user', (req, res) => {
+    res.send(`User id is ${req.query.id}`)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+
+
